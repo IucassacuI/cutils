3c3c0a31e435cc70c507b8816f27f8af531fbb43
Lucas <115167629+IucassacuI@users.noreply.github.com>
2024-03-14 17:58:44 -0300

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,14 +1,18 @@
 #ifndef CUTILS_MEMORY
 #define CUTILS_MEMORY
+
 #include <stdbool.h>
 #include <stdio.h>
+#include <signal.h>
+
+#define mem_at(addr, x, y) addr[y < mem_query((char *) addr)/x && y >= 0 ? y : raise(SIGABRT)]
 
 void mem_init(void);
 void mem_freeall(bool dealloc);
-char *mem_alloc(unsigned int bytes);
-char *mem_ralloc(char *ptr, unsigned int bytes);
+char *mem_alloc(int bytes);
+char *mem_ralloc(char *ptr, int bytes);
 void mem_copy(char *dst, const char *src);
 int mem_read(char *dst, FILE *src);
-int mem_query(const char *buffer);
+int mem_query(const void *buffer);
 
 #endif
