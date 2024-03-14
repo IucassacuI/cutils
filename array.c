e8e9177dc8c4291b6d66605162a8a7484d05e350
Lucas <115167629+IucassacuI@users.noreply.github.com>
2024-03-14 18:02:58 -0300

    Update array.c

--- array.c
+++ array.c
@@ -19,7 +19,7 @@ array arr_insert(array arr, uint16_t pos, uint64_t elm){
 		arr.top = pos;
 	}
 
-	arr.ptr[pos] = elm;
+	mem_at(arr.ptr, sizeof(uint64_t), pos) = elm;
 
 	arr.top += arr.top == pos;
 
@@ -31,18 +31,18 @@ uint64_t arr_at(array arr, uint16_t pos){
 	if(pos > arr.top)
 		return -1;
 
-	return arr.ptr[pos];
+	return mem_at(arr.ptr, sizeof(uint64_t), pos);
 }
 
 uint64_t arr_next(array *arr){
 	uint64_t elm = 0;
 
 	if(arr->iterator < arr->top){
-		elm = arr->ptr[arr->iterator];
+		elm = mem_at(arr->ptr, sizeof(uint64_t), arr->iterator);
 		arr->iterator++;
 	} else {
 		arr->iterator = 0;
-		elm = arr->ptr[0];
+		elm = mem_at(arr->ptr, sizeof(uint64_t), 0);
 	}
 
 	return elm;
@@ -54,7 +54,7 @@ array arr_push(array arr, uint64_t elm){
 	if(arr.top+1 > size)
 		arr.ptr = (uint64_t *) mem_ralloc((char *) arr.ptr, (size+1) * sizeof(uint64_t));
 
-	arr.ptr[arr.top] = elm;
+	mem_at(arr.ptr, sizeof(uint64_t), arr.top) = elm;
 	arr.top++;
 	arr.sorted = arr_sorted(arr);
 
@@ -62,7 +62,7 @@ array arr_push(array arr, uint64_t elm){
 }
 
 array arr_pop(array arr){
-	arr.ptr[arr.top-1] = 0;
+	mem_at(arr.ptr, sizeof(uint64_t), arr.top-1) = 0;
 	arr.top--;
 
 	return arr;
@@ -83,8 +83,13 @@ array arr_del(array arr, uint16_t pos){
 }
 
 array arr_shift(array arr){
-	arr.top++;
-	return arr;
+	array newarr = arr_new();
+
+	for(int i = 1; i < arr.top; i++){
+		newarr = arr_push(newarr, mem_at(arr.ptr, sizeof(uint64_t), i));
+	}
+
+	return newarr;
 }
 
 array arr_unshift(array arr, uint64_t elm){
@@ -93,7 +98,7 @@ array arr_unshift(array arr, uint64_t elm){
 	newarr = arr_push(newarr, elm);
 
 	for(int i = 0; i < arr.top; i++){
-		newarr = arr_push(newarr, arr.ptr[i]);
+		newarr = arr_push(newarr, mem_at(arr.ptr, sizeof(uint64_t), i));
 	}
 
 	return newarr;
@@ -108,7 +113,7 @@ static int cmp(const void *a, const void *b){
 
 static int arr_linear(array arr, uint64_t elm){
 	for(int j = 0; j < arr.top; j++){
-		if(arr.ptr[j] == elm)
+		if(mem_at(arr.ptr, sizeof(uint64_t), j) == elm)
 			return 1;
 	}
 
@@ -139,7 +144,7 @@ void arr_print(array arr){
 	putchar('[');
 
 	for(int k = 0; k < arr.top; k++){
-       		printf("%lu", arr.ptr[k]);
+       		printf("%lu", mem_at(arr.ptr, sizeof(uint64_t), k));
 
 		if(k+1 < arr.top)
 			printf(", ");
@@ -160,7 +165,7 @@ array arr_sort(array arr){
 
 int arr_sorted(array arr){
 	for(int i = 0; i < arr.top-1; i++){
-		if(arr.ptr[i] > arr.ptr[i+1])
+		if(mem_at(arr.ptr, sizeof(uint64_t), i) > mem_at(arr.ptr, sizeof(uint64_t), i+1))
 			return 0;
 	}
 
